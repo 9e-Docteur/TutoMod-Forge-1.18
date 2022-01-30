fb566ebcfa20ea86a00074c31c8de7eef49d5e30
Loris P <74488745+9e-Docteur@users.noreply.github.com>
2022-01-30 16:12:46 +0100

    Items

---
+++ src/main/java/fr/ninedocteur/tutomod/item/TutoItem.java
@@ -0,0 +1,20 @@
+package fr.ninedocteur.tutomod.item;
+
+import fr.ninedocteur.tutomod.TutoMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class TutoItem {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TutoMod.MOD_ID);
+
+    public static final RegistryObject<Item> TUTO_ITEM = ITEMS.register("tuto_item", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
